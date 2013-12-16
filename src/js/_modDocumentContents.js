fa948a2c9afe28a6e72ed534a0b6723d973f846d
Tomoya Koyanagi <tomk79@gmail.com>
2013-12-16 10:23:59 +0900

    UI上でだけ、増やすと減らすができるようになった。
    - 要素の中身はつくられてないけど。

--- src/js/_modDocumentContents.js
+++ src/js/_modDocumentContents.js
@@ -32,27 +32,31 @@
 	 * ドキュメントモジュール: ビュー
 	 */
 	contConteditor.cls.views.documentContent = Backbone.View.extend({
-		tagName: 'li',
+		tagName: 'div',
 		initialize: function() {
-			// this.model.on('destroy', this.remove, this);
+			this.model.on('destroy', this.remove, this);
 			// this.model.on('change', this.render, this);
 		},
 		events: {
-			// 'click .delete': 'destroy',
-			// 'click .toggle': 'toggle'
+			// 'click .toggle': 'toggle',
+			'click .cont_docCont_delete': 'destroy'
 		},
 		toggle: function() {
 			// this.model.set('completed', !this.model.get('completed'));
 		},
 		destroy: function() {
-			// if (confirm('are you sure?')) {
-			// 	this.model.destroy();
-			// }
+			// 要素を削除する
+			this.model.destroy();
 		},
 		remove: function() {
-			// this.$el.remove();
+			this.$el.remove();
 		},
-		template: _.template('<p>TEST: <%- name %></p>'),
+		template: _.template(
+			  '<div>'
+			+ 'TEST: <%- name %>'
+			+ '<button class="cont_docCont_delete">delete</button>'
+			+ '</div>'
+		),
 		render: function() {
 			var template = this.template(this.model.toJSON());
 			this.$el.html(template);
@@ -64,21 +68,40 @@
 	 * ドキュメントモジュール: コレクションビュー
 	 */
 	contConteditor.cls.views.documentContents = Backbone.View.extend({
-		tagName: 'ul',
+		tagName: 'div',
 		initialize: function() {
-			// this.collection.on('add', this.addNew, this);
-		},
+			this.collection.on('add', this.addNew, this);
+		} ,
+
 		addNew: function(docMod) {
-			// var docModView = new contConteditor.cls.views.documentModule({model: docMod});
-			// this.$el.append(docModView.render().el);
-		},
+			// alert('ここで増やす処理。');
+			var docModView = new contConteditor.cls.views.documentContent({model: docMod});
+			this.$el.append(docModView.render().el);
+		} ,
+
 		render: function() {
 			this.collection.each(function(docMod) {
-				var docModView = new contConteditor.cls.views.documentModule({model: docMod});
-				this.$el.append(docModView.render().el);
+				var docModView = new contConteditor.cls.views.documentContent( {model: docMod} );
+				this.$el.append( docModView.render().el );
 			}, this);
+			this.$el.append(
+				  '<div class="cont_form_addElement">'
+				+ '<select name="cont_form_addElement_modId"></select>'
+				+ '<button href="javascript:;" class="cont_addNew">要素を増やす</button>'
+				+ '</div>'
+			);
 			return this;
+		} ,
+
+		events: {
+			// 'click .delete': 'destroy',
+			'click .cont_addNew': 'addNewtest'
+		},
+		addNewtest: function(){
+			// alert('増やします。');
+			this.collection.add({});
 		}
+
 	});
 
 
